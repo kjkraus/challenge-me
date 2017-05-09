3dfeec30e45ad3785efdc97b0f56d9520e388cd3
Kent Kraus <Kent.Kraus@greenwayhealth.com>
2017-05-08 21:36:01 -0400

    commit create AccountStructArray

---
+++ ChallengeMe/ChallengeMe/AccountStructureArray.cs
@@ -0,0 +1,36 @@
+﻿namespace ChallengeMe
+{
+    internal enum AccountState
+    {
+        New,
+        Active,
+        UnderReview,
+        Frozen,
+        Closed
+    };
+
+    internal struct Account
+    {
+        public AccountState State;
+        public string Name;
+        public string Location;
+        public int AccountNumber;
+        public int CreatedChallenge;
+        public int AcceptedChallenge;
+    }
+
+    internal class AccountStructureArray
+    {
+        public static void Main()
+        {
+            const int MAX_PLAYERS = 100;
+            Account[] Player = new Account[MAX_PLAYERS];
+            Player[0].Name = "Aura";
+            Player[0].State = AccountState.Active;
+            Player[0].CreatedChallenge = 20;
+            Player[1].Name = "John";
+            Player[1].State = AccountState.Frozen;
+            Player[1].CreatedChallenge = 0;
+        }
+    }
+}
