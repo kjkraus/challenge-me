93fc8c9fabf70be361fb813c825c01e7f47197a3
Kent Kraus <Kent.Kraus@greenwayhealth.com>
2017-05-09 21:08:18 -0400

    add PrintAccount method

--- ChallengeMe/ChallengeMe/AccountStructureArray.cs
+++ ChallengeMe/ChallengeMe/AccountStructureArray.cs
@@ -1,4 +1,6 @@
-﻿namespace ChallengeMe
+﻿using System;
+
+namespace ChallengeMe
 {
     internal enum AccountState
     {
@@ -21,16 +23,28 @@
 
     internal class AccountStructureArray
     {
+        public static void PrintAccount(Account a)
+        {
+            Console.WriteLine("Name: " + a.Name);
+            Console.WriteLine("Location: " + a.Location);
+            Console.WriteLine("Created Challenges: " + a.CreatedChallenge);
+        }
+
         public static void Main()
         {
             const int MAX_PLAYERS = 100;
             Account[] Player = new Account[MAX_PLAYERS];
             Player[0].Name = "Aura";
+            Player[0].Location = "Tampa, FL";
             Player[0].State = AccountState.Active;
             Player[0].CreatedChallenge = 20;
+            PrintAccount(Player[0]);
+
             Player[1].Name = "John";
+            Player[1].Location = "Clearwater, FL";
             Player[1].State = AccountState.Frozen;
             Player[1].CreatedChallenge = 0;
+            PrintAccount(Player[1]);
         }
     }
 }
